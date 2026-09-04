2e54d9e6acba85b9aae846cd2adce0fbdd6b37fb
Vaibhav Bansal <vaibhav.bansal@uniqgridcloud.com>
2026-09-04 17:26:05 +0530

    DRL_1 completed

--- DRL_1/main.asm
+++ DRL_1/main.asm
@@ -8,6 +8,7 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
+	.globl _delay
 ;--------------------------------------------------------
 ; ram data
 ;--------------------------------------------------------
@@ -85,21 +86,112 @@ __sdcc_program_startup:
 ; code
 ;--------------------------------------------------------
 	.area CODE
-;	src/main.c: 7: void main(void)
+;	src/main.c: 17: void delay(uint32_t count)
+;	-----------------------------------------
+;	 function delay
+;	-----------------------------------------
+_delay:
+	sub	sp, #4
+;	src/main.c: 19: while (count--)
+	ldw	x, (0x07, sp)
+00101$:
+	ldw	(0x01, sp), x
+	ld	a, (0x09, sp)
+	ld	(0x03, sp), a
+	ld	a, (0x0a, sp)
+	ldw	y, (0x09, sp)
+	subw	y, #0x0001
+	ldw	(0x09, sp), y
+	jrnc	00123$
+	decw	x
+00123$:
+	tnz	a
+	jrne	00124$
+	ldw	y, (0x02, sp)
+	jrne	00124$
+	tnz	(0x01, sp)
+	jreq	00104$
+00124$:
+;	src/main.c: 21: __asm__("nop");
+	nop
+	jra	00101$
+00104$:
+;	src/main.c: 23: }
+	ldw	x, (5, sp)
+	addw	sp, #10
+	jp	(x)
+;	src/main.c: 25: void main(void)
 ;	-----------------------------------------
 ;	 function main
 ;	-----------------------------------------
 _main:
-;	src/main.c: 10: PC_DDR |= (1 << 4);
+	sub	sp, #3
+;	src/main.c: 28: uint8_t stateChanged = 1;
+	ld	a, #0x01
+	ld	(0x01, sp), a
+;	src/main.c: 30: PC_DDR |= (1 << 4);
 	bset	0x500c, #4
-;	src/main.c: 13: PC_CR1 |= (1 << 4);
+;	src/main.c: 31: PC_DDR &= ~(1 << 3); /* PC3 as input */
+	bres	0x500c, #3
+;	src/main.c: 34: PC_CR1 |= (1 << 4);
 	bset	0x500d, #4
-;	src/main.c: 16: PC_ODR |= (1 << 4);
+;	src/main.c: 35: PC_CR1 |= (1 << 3); /* PC3 as input pullup */
+	bset	0x500d, #3
+;	src/main.c: 39: while (1)
+00108$:
+;	src/main.c: 42: Ip_Pin_state = (PC_IDR >> Ip_Pin) & 0x01; /* Read PC3 state */
+	ld	a, 0x500b
+	swap	a
+	sll	a
+	clr	a
+	rlc	a
+;	src/main.c: 43: if (Ip_Pin_state == 1 && stateChanged == 1) /* If PC3 is LOW */
+	ld	(0x02, sp), a
+	dec	a
+	jrne	00103$
+	ld	a, (0x01, sp)
+	dec	a
+	jrne	00103$
+;	src/main.c: 45: stateChanged = 0;
+	clr	(0x01, sp)
+;	src/main.c: 46: for (uint32_t i = 0; i < 6; i++)
+	clr	(0x03, sp)
+00111$:
+	ld	a, (0x03, sp)
+	cp	a, #0x06
+	jrnc	00103$
+;	src/main.c: 48: PC_ODR |= (1 << 4); /* Set PC4 HIGH */
 	bset	0x500a, #4
-;	src/main.c: 18: while (1)
-00102$:
-	jra	00102$
-;	src/main.c: 22: }
+;	src/main.c: 49: delay(DELAY_500MS_COUNT); /* Delay for a while */
+	push	#0x10
+	push	#0x27
+	clrw	x
+	pushw	x
+	call	_delay
+;	src/main.c: 50: PC_ODR &= ~(1 << 4); /* Set PC4 LOW */
+	bres	0x500a, #4
+;	src/main.c: 51: delay(DELAY_500MS_COUNT); /* Delay for a while */
+	push	#0x10
+	push	#0x27
+	clrw	x
+	pushw	x
+	call	_delay
+;	src/main.c: 46: for (uint32_t i = 0; i < 6; i++)
+	inc	(0x03, sp)
+	jra	00111$
+00103$:
+;	src/main.c: 54: if (Ip_Pin_state == 0) /* If PC3 is HIGH */
+	tnz	(0x02, sp)
+	jrne	00106$
+;	src/main.c: 56: stateChanged = 1;
+	ld	a, #0x01
+	ld	(0x01, sp), a
+00106$:
+;	src/main.c: 59: PC_ODR |= (1 << 4); /* Set PC4 HIGH */
+	bset	0x500a, #4
+	jra	00108$
+;	src/main.c: 61: }
+	addw	sp, #3
 	ret
 	.area CODE
 	.area CONST
--- DRL_1/src/main.c
+++ DRL_1/src/main.c
@@ -1,22 +1,61 @@
 #include <stdint.h>
 
 #define PC_ODR  (*(volatile uint8_t *)0x500A)
+#define PC_IDR  (*(volatile uint8_t *)0x500B)
 #define PC_DDR  (*(volatile uint8_t *)0x500C)
 #define PC_CR1  (*(volatile uint8_t *)0x500D)
 
+#define CLK_FREQ 2000000UL
+#define CLK_CONT_PER_LOOP   4
+#define TIME_PER_LOOP_US   (CLK_CONT_PER_LOOP * 1000000UL / CLK_FREQ)
+#define DELAY_500MS_COUNT   (20000UL / TIME_PER_LOOP_US)
+
+
+#define Op_Pin  4
+#define Ip_Pin  3
+
+void delay(uint32_t count)
+{
+    while (count--)
+    {
+        __asm__("nop");
+    }
+}
+
 void main(void)
 {
+    uint8_t Ip_Pin_state = 0;
+    uint8_t stateChanged = 1;
     /* PC4 as output */
     PC_DDR |= (1 << 4);
+    PC_DDR &= ~(1 << 3); /* PC3 as input */
 
     /* Push-pull output */
     PC_CR1 |= (1 << 4);
+    PC_CR1 |= (1 << 3); /* PC3 as input pullup */
 
     /* Set PC4 HIGH */
-    PC_ODR |= (1 << 4);
 
     while (1)
     {
         /* Keep PC4 HIGH */
+        Ip_Pin_state = (PC_IDR >> Ip_Pin) & 0x01; /* Read PC3 state */
+        if (Ip_Pin_state == 1 && stateChanged == 1) /* If PC3 is LOW */
+        {
+            stateChanged = 0;
+            for (uint32_t i = 0; i < 6; i++)
+            {
+                PC_ODR |= (1 << 4); /* Set PC4 HIGH */
+                delay(DELAY_500MS_COUNT); /* Delay for a while */
+                PC_ODR &= ~(1 << 4); /* Set PC4 LOW */
+                delay(DELAY_500MS_COUNT); /* Delay for a while */
+            }
+        }
+        if (Ip_Pin_state == 0) /* If PC3 is HIGH */
+        {
+            stateChanged = 1;
+        }
+            
+        PC_ODR |= (1 << 4); /* Set PC4 HIGH */
     }
 }
